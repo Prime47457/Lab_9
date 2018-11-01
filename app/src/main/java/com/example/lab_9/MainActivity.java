ff9593d1fd4cb732b74dc688ed76f836f306b82f
Rachata Pianpucktr <primepianpucktr@gmail.com>
2018-11-01 23:44:37 +0700

    test change nothing kae tum perm

--- app/src/main/java/com/example/lab_9/MainActivity.java
+++ app/src/main/java/com/example/lab_9/MainActivity.java
@@ -2,6 +2,14 @@ package com.example.lab_9;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.Retrofit;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public class MainActivity extends AppCompatActivity {
 
